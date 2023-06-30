baf7543cb284e72d48f6cff2f8f0c80563037f12
Nathaniel Heintz <Ethrel@hotmail.ca>
2023-06-29 20:10:04 -0600

    Update logo sizes and add logo source files

---
+++ Logo/size.bat
@@ -0,0 +1,10 @@
+magick convert Logo-192x192.png -resize 144x Logo-144x144.png
+magick convert Logo-192x192.png -resize 96x Logo-96x96.png
+magick convert Logo-192x192.png -resize 72x Logo-72x72.png
+magick convert Logo-192x192.png -resize 48x Logo-48x48.png
+
+cp Logo-192x192.png ../android/app/src/main/res/mipmap-xxxhdpi/logo.png
+mv Logo-144x144.png ../android/app/src/main/res/mipmap-xxhdpi/logo.png
+mv Logo-96x96.png ../android/app/src/main/res/mipmap-xhdpi/logo.png
+mv Logo-72x72.png ../android/app/src/main/res/mipmap-hdpi/logo.png
+mv Logo-48x48.png ../android/app/src/main/res/mipmap-mdpi/logo.png
