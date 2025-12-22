aa0c673d1add721122aa5bee37af5f5f11ec6872
Nathaniel Heintz <Ethrel@hotmail.ca>
2025-12-22 07:39:50 -0700

    build fail test ... 9?
    * Update build.gradle.kts
    
    * Update build.yml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,9 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        ndk {
+            debugSymbolLevel = 'FULL'
+        }
     }
     signingConfigs {
         create("release") {
