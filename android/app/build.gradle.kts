bb401fbc97baa7232b3a77dd9f4c3afce00c5280
Nathaniel Heintz <Ethrel@hotmail.ca>
2025-12-22 07:11:41 -0700

    Update build.gradle.kts (#24)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.neptunes_pride_agent_mobile"
+    namespace = "net.ethrel.npa_mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "net.ethrel.neptunes_pride_agent_mobile"
+        applicationId = "net.ethrel.npa_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
